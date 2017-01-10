46f456c5b6c81ccff69c8fc7c999a92b25c0ccaa
Erik Hedberg <erik.hedberg@nordnet.se>
2017-01-10 19:42:35 +0100

    Move comics to separate file

---
+++ comics.js
@@ -0,0 +1,50 @@
+const cheerio = require('cheerio');
+
+module.exports = {
+  'ballard-street': {
+    url: 'http://www.gocomics.com/ballardstreet/',
+    scraper: (body) => {
+      const $ = cheerio.load(body);
+      const url = $('.item-comic-image').children('img').first().attr('src');
+      return url;
+    },
+    contentType: 'jpeg',
+  },
+  dilbert: {
+    url: 'http://dilbert.com/',
+    scraper: (body) => {
+      const $ = cheerio.load(body);
+      const url = $('.img-comic').first().attr('src');
+      return url;
+    },
+    contentType: 'jpeg',
+  },
+  halge: {
+    url: 'http://www.halge.se/strippar/',
+    scraper: (body) => {
+      const $ = cheerio.load(body);
+      const url = $('.entry-featured-image-url').first().children('img').first().attr('src');
+      return url;
+    },
+    contentType: 'jpeg',
+  },
+  rocky: {
+    url: 'http://www.dn.se/serier/rocky/',
+    scraper: (body) => {
+      const $ = cheerio.load(body);
+      const relativeUrl = $('#article-content').find('a[href$="gif"]').attr('href');
+      const url = `http://www.dn.se/${relativeUrl}`;
+      return url;
+    },
+    contentType: 'image/gif',
+  },
+  userfriendly: {
+    url: 'http://userfriendly.org/',
+    scraper: (body) => {
+      const $ = cheerio.load(body);
+      const url = $('[alt="Latest Strip"]').first().attr('src');
+      return url;
+    },
+    contentType: 'jpeg',
+  },
+};
--- index.js
+++ index.js
@@ -1,81 +1,13 @@
-const cheerio = require('cheerio');
 const express = require('express');
 const request = require('request');
+const comics = require('./comics');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36';
 
-const comics = {
-  'ballard-street': {
-    url: 'http://www.gocomics.com/ballardstreet/',
-    scraper: (body) => {
-      const $ = cheerio.load(body);
-      const url = $('.item-comic-image').children('img').first().attr('src');
-      return url;
-    },
-    contentType: 'jpeg',
-  },
-  dilbert: {
-    url: 'http://dilbert.com/',
-    scraper: (body) => {
-      const $ = cheerio.load(body);
-      const url = $('.img-comic').first().attr('src');
-      return url;
-    },
-    contentType: 'jpeg',
-  },
-  halge: {
-    url: 'http://www.halge.se/strippar/',
-    scraper: (body) => {
-      const $ = cheerio.load(body);
-      const url = $('.entry-featured-image-url').first().children('img').first().attr('src');
-      return url;
-    },
-    contentType: 'jpeg',
-  },
-  rocky: {
-    url: 'http://www.dn.se/serier/rocky/',
-    scraper: (body) => {
-      const $ = cheerio.load(body);
-      const relativeUrl = $('#article-content').find('a[href$="gif"]').attr('href');
-      const url = `http://www.dn.se/${relativeUrl}`;
-      return url;
-    },
-    contentType: 'image/gif',
-  },
-  userfriendly: {
-    url: 'http://userfriendly.org/',
-    scraper: (body) => {
-      const $ = cheerio.load(body);
-      const url = $('[alt="Latest Strip"]').first().attr('src');
-      return url;
-    },
-    contentType: 'jpeg',
-  },
-};
-
-const getLatestStripUrl = (userAgent, url, scraper) => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      url,
-      headers: {
-        'User-Agent': userAgent,
-      }
-    };
-
-    request(options, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        reject(error);
-      }
-
-      resolve(scraper(body));
-    });
-  });
-}
-
-const getImageData = (userAgent, url) => {
+const requestBody = (userAgent, url) => {
   return new Promise((resolve, reject) => {
     const options = {
       url,
@@ -110,8 +42,9 @@ app.get('/api/v1/comics/:slug/latest', comicValidator, (req, res) => {
   const slug = req.params.slug;
   const comic = comics[slug];
 
-  getLatestStripUrl(userAgent, comic.url, comic.scraper)
-    .then((url) => getImageData(userAgent, url))
+  requestBody(userAgent, comic.url)
+    .then(comic.scraper)
+    .then((url) => requestBody(userAgent, url))
     .then((imageData) => {
       res.writeHead(200, { 'Content-Type': comic.contentType });
       res.end(imageData, 'binary');
@@ -123,7 +56,8 @@ app.get('/api/v1/comics/:slug/latest/url', comicValidator, (req, res) => {
   const slug = req.params.slug;
   const comic = comics[slug];
 
-  getLatestStripUrl(userAgent, comic.url, comic.scraper)
+  requestBody(userAgent, comic.url)
+    .then(comic.scraper)
     .then((url) => {
       res.send(url);
     })
